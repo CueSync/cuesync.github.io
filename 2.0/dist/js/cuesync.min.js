ab1e43b5bff1a5cbedfc1fadf4f90f81a781aa47
Neeraj Kumar Das <nkdas91@gmail.com>
2025-04-25 18:33:39 +0530

    Version 2.0.0-alpha2

--- 2.0/dist/js/cuesync.min.js
+++ 2.0/dist/js/cuesync.min.js
@@ -1,5 +1,5 @@
 /*!
-  * CueSync v2.0.0-alpha1 (https://cuesync.github.io/)
+  * CueSync v2.0.0-alpha2 (https://cuesync.github.io/)
   * Copyright 2025 Neeraj Kumar Das (https://github.com/nkdas91)
   * Licensed under MIT (https://github.com/cuesync/cuesync/blob/master/LICENSE)  
   */
